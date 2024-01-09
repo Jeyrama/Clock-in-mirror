c9d6660e9a833499a8be2be4e67afba505b1648b
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-01-09 14:08:26 -0800

    Create Clock-in-mirror.js

---
+++ Clock-in-mirror.js
@@ -0,0 +1,18 @@
+/*
+Peter is sitting in his office and can see a clock in the mirror.
+He knows that when the clock shows 12:22, the time is 11:38
+
+in the same manner:
+  05:25 --> 06:35
+  01:50 --> 10:10
+  11:58 --> 12:02
+  12:01 --> 11:59
+
+Please complete the function WhatIsTheTime(timeInMirror), 
+where timeInMirror is the mirrored time (what Peter sees) as string.
+
+Return the real time as a string.
+Consider hours to be between 1 <= hour < 13.
+So there is no 00:20, instead it is 12:20.
+There is no 13:20, instead it is 01:20.
+*/
